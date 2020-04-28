ca27afe120ef797fd1ccbea3eeddfdecef28a1ad
mHALr <ryo97321@gmail.com>
2020-04-29 07:04:29 +0900

    add intro_python3/

---
+++ intro_python3/chap4/ex4_10.py
@@ -0,0 +1,15 @@
+def test(func):
+    def new_func(*args, **kwargs):
+        print('start')
+        result = func(*args, **kwargs)
+        print('end')
+        return result
+    return new_func
+
+
+@test
+def greeting():
+    print('Greetings, Earthling')
+
+
+greeting()
---
+++ intro_python3/chap4/ex4_12.py
@@ -0,0 +1,5 @@
+titles = ['Creature of Habit', 'Crewel Fate']
+plots = ['A nun turns into a monster', 'A haunted yarn shop']
+
+movies = dict(zip(titles, plots))
+print(movies)
