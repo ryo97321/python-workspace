dddcd7f779e7ef3477737b871bd71d8ae09ed4ce
mHALr <ryo97321@gmail.com>
2020-05-06 09:31:23 +0900

    add more redis files

---
+++ intro_python3/chap11/redis_dryer2.py
@@ -0,0 +1,25 @@
+def dryer():
+    import redis
+    import os
+    import time
+    conn = redis.Redis()
+    pid = os.getpid()
+    timeout = 20
+    print('Dryer process %s is starting' % pid)
+    while True:
+        msg = conn.blpop('dishes', timeout)
+        if not msg:
+            break
+        val = msg[1].decode('utf-8')
+        if val == 'quit':
+            break
+        print('%s dried %s' % (pid, val))
+        time.sleep(0.1)
+    print('Dryer process %s is done' % pid)
+
+if __name__ == '__main__':
+    import multiprocessing
+    DRYERS = 3
+    for num in range(DRYERS):
+        p = multiprocessing.Process(target=dryer)
+        p.start()
