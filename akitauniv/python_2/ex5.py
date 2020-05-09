e7156f9d4a569768e11bacb3e3a4fc46defff88c
mHALr <ryo97321@gmail.com>
2020-05-09 17:07:06 +0900

    change to GUI

--- akitauniv/python_2/ex5.py
+++ akitauniv/python_2/ex5.py
@@ -2,6 +2,7 @@
 
 import csv
 from collections import defaultdict
+import PySimpleGUI as sg
 
 # ある日・都道府県の感染者数（ex. PREF_DAY_INFECTED[('岩手県', '1/20/2020')] = 0）
 PREF_DAY_INFECTED = defaultdict(int)
@@ -31,16 +32,50 @@ def make_dict():
             PREF_TOTAL_INFECTED[pref] += 1
 
 
+def parse_date(date):
+    '''dateを辞書用に変換する（2020/4/30 -> 4/30/2020）'''
+    date_parts = date.split('/')
+    year = date_parts[0]
+    month = date_parts[1]
+    day = date_parts[2]
+
+    return month + '/' + day + '/' + year
+
+
 if __name__ == '__main__':
     make_dict()
 
-    print('どちらを表示しますか？')
-    option = int(input('0. ある日のある都道府県の感染者数 / 1. ある都道府県の合計感染者数：'))
+    sg.theme('Dark Blue 3')
+    layout = [
+        [sg.Text('日付', size=(15, 1), key='-DATE_TEXT-'), \
+            sg.InputText('2020/4/30', key='-DATE_INPUTTEXT-')],
+        [sg.Text('都道府県名', size=(15, 1), key='-PREF_TEXT-'), \
+            sg.InputText('東京都', key='-PREF_INPUTTEXT-')],
+        [sg.Submit(button_text='表示')]
+    ]
 
-    input_pref = input('都道府県名（ex. 秋田県）：')
-    if option == 0:
-        input_date = input('日時（ex. 1/20/2020）：')
-        tup = (input_pref, input_date)
-        print("\n{} : {} の感染者数は {}人 です".format(input_date, input_pref, PREF_DAY_INFECTED[tup]))
-    elif option == 1:
-        print('\n{} の合計感染者数は {}人 です'.format(input_pref, PREF_TOTAL_INFECTED[input_pref]))
+    window = sg.Window('感染者数表示アプリ', layout)
+
+    while True:
+        event, values = window.read()
+        if event is None:
+            break
+
+        if event == '表示':
+            date_obj = window['-DATE_INPUTTEXT-']
+            pref_obj = window['-PREF_INPUTTEXT-']
+
+            date_obj_value = date_obj.get()
+            pref_obj_value = pref_obj.get()
+
+            if date_obj_value == '':
+                show_message = pref_obj_value + 'の合計感染者数は ' \
+                    + str(PREF_TOTAL_INFECTED[pref_obj_value]) + 'です。'
+            else:
+                key = (pref_obj_value, parse_date(date_obj_value))
+                show_message = date_obj_value + ' : ' + pref_obj_value \
+                    + ' の感染者数は ' + str(PREF_DAY_INFECTED[key]) + 'です。'
+
+            sg.popup(show_message)
+
+    window.close()
